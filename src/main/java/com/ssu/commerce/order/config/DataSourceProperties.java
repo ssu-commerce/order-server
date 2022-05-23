415a62beb6528164c96a7861b9f0e0bb3010d183
hanjoo96.cho <hanjoo96.cho@navercorp.com>
2022-05-23 22:53:11 +0900

    #1 core 버전업으로 적용

--- src/main/java/com/ssu/commerce/order/config/DataSourceProperties.java
+++ src/main/java/com/ssu/commerce/order/config/DataSourceProperties.java
@@ -1,5 +1,7 @@
 package com.ssu.commerce.order.config;
 
+import com.ssu.commerce.core.configs.AbstractDataSourceProperties;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,7 +14,7 @@ import org.springframework.vault.annotation.VaultPropertySources;
         @VaultPropertySource(value = "ssu-commerce-order/${spring.profiles.active:dev}", propertyNamePrefix = "ssu-commerce-order."),
         @VaultPropertySource(value = "ssu-commerce-order/dev", propertyNamePrefix = "ssu-commerce-order.")
 })
-public class DataSourceProperties {
+public class DataSourceProperties implements AbstractDataSourceProperties {
     @Value("${ssu-commerce-order.dataSource}")
     public String dataSource;
 
@@ -24,4 +26,60 @@ public class DataSourceProperties {
 
     @Value("${ssu-commerce-order.driverClassName}")
     public String  driverClassName;
+
+
+    @NotNull
+    @Override
+    public String getDataSource() {
+        return this.dataSource;
+    }
+
+    @Override
+    public void setDataSource(@NotNull String s) {
+
+    }
+
+    @NotNull
+    @Override
+    public String getDriverClassName() {
+        return this.driverClassName;
+    }
+
+    @Override
+    public void setDriverClassName(@NotNull String s) {
+
+    }
+
+    @NotNull
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public void setPassword(@NotNull String s) {
+
+    }
+
+    @NotNull
+    @Override
+    public String getProjectName() {
+        return "order";
+    }
+
+    @Override
+    public void setProjectName(@NotNull String s) {
+
+    }
+
+    @NotNull
+    @Override
+    public String getUserId() {
+        return this.userId;
+    }
+
+    @Override
+    public void setUserId(@NotNull String s) {
+
+    }
 }
