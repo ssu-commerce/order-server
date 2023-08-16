498b88678c27fad8e3d99409030446d2becafa3a
JaeSung Choi <ky4171@naver.com>
2023-08-16 17:56:00 +0900

    grpc 연결 poc

---
+++ src/main/java/com/ssu/commerce/order/GrpcServerService.java
@@ -0,0 +1,22 @@
+package com.ssu.commerce.order;
+
+import com.ssu.commerce.grpc.HelloReply;
+import com.ssu.commerce.grpc.HelloRequest;
+import com.ssu.commerce.grpc.SimpleGrpc;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+@Slf4j
+public class GrpcServerService extends SimpleGrpc.SimpleImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        log.info("[GrpcTest] HelloRequest : {}", request);
+        HelloReply reply = HelloReply.newBuilder()
+                .setMessage("Hello ==> " + request.getName())
+                .build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
