de03456e8af75b73b1bae83a0fff071fa1d8e349
JaeSung Choi <ky4171@naver.com>
2023-10-22 17:27:50 +0900

    gRPC 수정내용

--- api/src/main/java/com/ssu/commerce/order/grpc/GetAvailableBookInfoGrpcService.java
+++ api/src/main/java/com/ssu/commerce/order/grpc/GetAvailableBookInfoGrpcService.java
@@ -1,7 +1,11 @@
 package com.ssu.commerce.order.grpc;
 
+import com.ssu.commerce.order.dto.request.RentalBookRequestDto;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GetAvailableBookInfoGrpcService {
@@ -15,30 +19,40 @@ public class GetAvailableBookInfoGrpcService {
     @GrpcClient("rollBackRental")
     private RollBackRentalGrpc.RollBackRentalBlockingStub rollBackRentalBlockingStub;
 
-    public RentalBookResponse sendMessageToGetRentalBook(final String bookId, final String token) {
-        return this.rentalBookBlockingStub.rentalBook(
+    public RentalBookResponse sendMessageToGetRentalBook(final List<RentalBookRequestDto> requestDto, String accessToken) {
+
+        return rentalBookBlockingStub.rentalBook(
                 RentalBookRequest.newBuilder()
-                        .setToken(token)
-                        .setId(bookId)
-                        .build()
+                        .setToken(accessToken)
+                        .addAllId(
+                                requestDto.stream().map(
+                                        req -> req.getBookId().toString()
+                                ).collect(Collectors.toList())
+                        ).build()
         );
     }
 
-    public CompleteRentalBookResponse sendMessageToCompleteRentalBook(final String bookId, final String token) {
-        return this.completeRentalBookBlockingStub.completeRentalBook(
+    public CompleteRentalBookResponse sendMessageToCompleteRentalBook(final List<RentalBookRequestDto> requestDto, String accessToken) {
+        return completeRentalBookBlockingStub.completeRentalBook(
                 CompleteRentalBookRequest.newBuilder()
-                        .setToken(token)
-                        .setId(bookId)
-                        .build()
+                        .setToken(accessToken)
+                        .addAllId(
+                                requestDto.stream().map(
+                                        req -> req.getBookId().toString()
+                                ).collect(Collectors.toList())
+                        ).build()
         );
     }
 
-    public RollBackBookResponse sendMessageToRollBackRental(final String bookId, final String token) {
-        return this.rollBackRentalBlockingStub.rollBackRental(
+    public RollBackBookResponse sendMessageToRollBackRental(final List<RentalBookRequestDto> requestDto, String accessToken) {
+        return rollBackRentalBlockingStub.rollBackRental(
                 RollBackBookRequest.newBuilder()
-                        .setToken(token)
-                        .setId(bookId)
-                        .build()
+                        .setToken(accessToken)
+                        .addAllId(
+                                requestDto.stream().map(
+                                        req -> req.getBookId().toString()
+                                ).collect(Collectors.toList())
+                        ).build()
         );
     }
 }
