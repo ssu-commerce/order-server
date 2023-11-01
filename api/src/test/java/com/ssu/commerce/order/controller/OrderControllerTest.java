26b22677e1fffd4a97650627d2b8da437d61a92f
JaeSung Choi <ky4171@naver.com>
2023-11-01 14:24:37 +0900

    #32 Order Controller test code

--- api/src/test/java/com/ssu/commerce/order/controller/OrderControllerTest.java
+++ api/src/test/java/com/ssu/commerce/order/controller/OrderControllerTest.java
@@ -2,6 +2,15 @@ package com.ssu.commerce.order.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.ssu.commerce.core.error.NotFoundException;
+import com.ssu.commerce.order.dto.param.GetOrderListParamDto;
+import com.ssu.commerce.order.dto.request.CreateOrderRequestDto;
+import com.ssu.commerce.order.dto.response.OrderListParamDto;
+import com.ssu.commerce.order.model.Order;
+import org.hamcrest.Matchers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import com.ssu.commerce.core.security.user.SsuCommerceAuthenticatedPrincipal;
 import com.ssu.commerce.order.service.OrderService;
@@ -20,8 +29,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(OrderController.class)
@@ -34,7 +51,7 @@ class OrderControllerTest implements OrderControllerTestDataSupplier {
     private ObjectMapper objectMapper;
     @MockBean
     private OrderService orderService;
-/*
+
     @BeforeEach
     void setAuthUser() {
         SsuCommerceAuthenticatedPrincipal principal = OrderControllerTestDataSupplier.getSsuCommerceAuthenticatedPrincipal();
@@ -43,24 +60,93 @@ class OrderControllerTest implements OrderControllerTestDataSupplier {
     }
 
     @Test
-    void rentalBook() {
-        UUID userId = TEST_VAL_USER_ID;
+    void createOrder_success() {
+        SsuCommerceAuthenticatedPrincipal principal = OrderControllerTestDataSupplier.getSsuCommerceAuthenticatedPrincipal();
+        UUID userId = principal.getUserId();
         UUID orderId = TEST_VAL_ORDER_ID;
+        String accessToken = principal.getAccessToken();
         Order order = OrderControllerTestDataSupplier.getOrder();
-        RentalBookRequestDto rentalBookRequestDto = OrderControllerTestDataSupplier.getRentalBookRequestDto();
+        List<CreateOrderRequestDto> requestDto = OrderControllerTestDataSupplier.getRentalBookRequestDto();
 
-        when(orderService.rentalBook(rentalBookRequestDto, userId)).thenReturn(order);
+        when(orderService.createOrder(eq(requestDto), eq(accessToken), eq(userId))).thenReturn(order);
 
         assertDoesNotThrow(() -> {
-            mockMvc.perform(post(OrderConstant.ORDER_API_BASE + "/book/rental")
-                            .content(objectMapper.writeValueAsString(rentalBookRequestDto))
+            mockMvc.perform(post("/api/v1/order")
+                            .content(objectMapper.writeValueAsString(requestDto))
                             .contentType(MediaType.APPLICATION_JSON)
                             .with(csrf())
                     .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.id", equalTo(orderId.toString())));
+                    .andExpect(jsonPath("$.id", Matchers.equalTo(orderId.toString())));
         });
 
-        verify(orderService, times(1)).rentalBook(rentalBookRequestDto, userId);
-    }*/
+        verify(orderService, times(1)).createOrder(any(), eq(accessToken), eq(userId));
+    }
+
+    @Test
+    void createOrder_badRequest_requestBody_is_null() {
+        assertDoesNotThrow(() -> {
+            mockMvc.perform(post("/api/v1/order")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .with(csrf())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+        });
+    }
+
+    @Test
+    void updateOrderItem_success() {
+        UUID orderItemId = TEST_VAL_ORDER_ITEM_ID;
+
+        when(orderService.updateOrderItem(orderItemId)).thenReturn(orderItemId);
+
+        assertDoesNotThrow(() -> {
+            mockMvc.perform(put("/api/v1/order/" + orderItemId)
+                            .with(csrf())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        });
+
+        verify(orderService, times(1)).updateOrderItem(orderItemId);
+    }
+
+    @Test
+    void updateOrderItem_fail_NotFoundException() {
+        UUID orderItemId = TEST_VAL_ORDER_ITEM_ID;
+        String errorMessage = String.format("orderItem not found; orderItemId=%s", orderItemId);
+        NotFoundException notFoundException = new NotFoundException(errorMessage, "ORDER_ITEM_001");
+
+        when(orderService.updateOrderItem(orderItemId)).thenThrow(notFoundException);
+
+        assertDoesNotThrow(() -> {
+            mockMvc.perform(put("/api/v1/order/" + orderItemId)
+                            .with(csrf())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.message", Matchers.equalTo(errorMessage)));
+        });
+
+        verify(orderService, times(1)).updateOrderItem(orderItemId);
+    }
+
+    @Test
+    void testGetOrder_success() {
+        GetOrderListParamDto getOrderListParamDto = OrderControllerTestDataSupplier.getGetOrderListParamDto();
+        Page<OrderListParamDto> orderListParamDto = new PageImpl<>(Arrays.asList(OrderControllerTestDataSupplier.getOrderListParamDto()));
+
+        when(orderService.getOrderList(
+                argThat(dto -> dto instanceof GetOrderListParamDto && dto.getUserId().equals(getOrderListParamDto.getUserId()
+                )))).thenReturn(orderListParamDto);
+
+        assertDoesNotThrow(() -> {
+            mockMvc.perform(get("/api/v1/order")
+                            .with(csrf())
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        });
+
+        verify(orderService, times(1))
+                .getOrderList(argThat(dto -> dto instanceof GetOrderListParamDto &&
+                        dto.getUserId().equals(getOrderListParamDto.getUserId())));
+    }
 }
--- api/src/test/java/com/ssu/commerce/order/supplier/OrderControllerTestDataSupplier.java
+++ api/src/test/java/com/ssu/commerce/order/supplier/OrderControllerTestDataSupplier.java
@@ -2,11 +2,15 @@ package com.ssu.commerce.order.supplier;
 
 import com.ssu.commerce.core.security.user.SsuCommerceAuthenticatedPrincipal;
 import com.ssu.commerce.core.security.user.UserRole;
+import com.ssu.commerce.order.dto.param.GetOrderListParamDto;
 import com.ssu.commerce.order.dto.request.CreateOrderRequestDto;
+import com.ssu.commerce.order.dto.response.OrderListParamDto;
 import com.ssu.commerce.order.model.Order;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderControllerTestDataSupplier {
@@ -15,13 +19,21 @@ public interface OrderControllerTestDataSupplier {
     UUID TEST_VAL_ANOTHER_BOOK_ID = UUID.fromString("8e4eed48-4f55-431b-99e2-64fd777fcf8b");
     UUID TEST_VAL_USER_ID = UUID.fromString("b94c78ae-542c-4f04-b392-4dd107252645");
     UUID TEST_VAL_ORDER_ID = UUID.fromString("3a88bd50-e2e5-4e17-9cb1-40324e93099b");
+    UUID TEST_VAL_ORDER_ITEM_ID = UUID.fromString("508dc153-b376-4693-acf1-aed1e61bb593");
 
-    static CreateOrderRequestDto getRentalBookRequestDto() {
-        return CreateOrderRequestDto.builder()
-                .bookId(TEST_VAL_BOOK_ID)
-                .startedAt(LocalDateTime.now())
-                .endAt(LocalDateTime.now().plusDays(10))
-                .build();
+    static List<CreateOrderRequestDto> getRentalBookRequestDto() {
+        return Arrays.asList(
+                CreateOrderRequestDto.builder()
+                        .bookId(TEST_VAL_BOOK_ID)
+                        .startedAt(LocalDateTime.now())
+                        .endAt(LocalDateTime.now().plusDays(10))
+                        .build(),
+                CreateOrderRequestDto.builder()
+                        .bookId(TEST_VAL_ANOTHER_BOOK_ID)
+                        .startedAt(LocalDateTime.now())
+                        .endAt(LocalDateTime.now().plusMonths(1))
+                        .build()
+        );
     }
 
     static Order getOrder() {
@@ -41,5 +53,20 @@ public interface OrderControllerTestDataSupplier {
                 ),
                 "USER_NAME"
         );
+    }
+
+    static GetOrderListParamDto getGetOrderListParamDto() {
+        return GetOrderListParamDto.builder()
+                .userId(TEST_VAL_USER_ID)
+                .pageable(Pageable.unpaged())
+                .build();
+    }
+
+    static OrderListParamDto getOrderListParamDto() {
+        return OrderListParamDto.builder()
+                .id(TEST_VAL_ORDER_ID)
+                .userId(TEST_VAL_USER_ID)
+                .orderedAt(LocalDateTime.now())
+                .build();
     }
 }
